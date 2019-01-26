f89c3d6acc7bed72825a52c251d1ee2c7d06c0ca
SchlachterSchmidt <teenageenemanursesinbondage@gmail.com>
2019-01-26 16:52:16 +0000

    type parameter

--- src/main/kotlin/Chapter6/6-13-TypeParameters.kt
+++ src/main/kotlin/Chapter6/6-13-TypeParameters.kt
@@ -1,2 +1,27 @@
 package Chapter6
 
+fun main() {
+
+
+    // T may be null, even though it has no question mark so we need to safe call the hashCode method
+    fun <T> printHashCode(t: T) {
+        println(t?.hashCode())
+    }
+
+    val test1: String? = null
+    val test2: String = "ASD"
+
+    printHashCode(test1)
+    printHashCode(test2)
+
+    // to get rid of this, we can specify a non-nullable upper bound
+    fun <T: Any> printHashCodeSafe(t: T) {
+        println(t.hashCode())
+    }
+
+    // this will cause a type mismatch error:
+    // printHashCodeSafe(test1)
+
+    // this works fine
+    printHashCodeSafe(test2)
+}

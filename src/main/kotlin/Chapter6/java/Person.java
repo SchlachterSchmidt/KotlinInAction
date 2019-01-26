4e423347ac5a01856a30b04fdab729e469aaba7f
SchlachterSchmidt <teenageenemanursesinbondage@gmail.com>
2019-01-26 16:52:27 +0000

    platform types

--- src/main/kotlin/Chapter6/java/Person.java
+++ src/main/kotlin/Chapter6/java/Person.java
@@ -1,4 +1,25 @@
 package Chapter6.java;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 public class Person {
+
+    private final String name;
+
+    @NotNull
+    private final Integer age;
+
+    @Nullable
+    private final String lastName;
+
+    public Person(String name, int age, String lastName) {
+        this.name = name;
+        this.age = age;
+        this.lastName = lastName;
+    }
+
+    public String getName() {
+        return name;
+    }
 }

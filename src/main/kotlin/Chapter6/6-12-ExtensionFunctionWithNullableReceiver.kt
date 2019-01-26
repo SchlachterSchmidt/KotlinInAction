e53a8f95de6e42270d938097227b73a199c9fe01
SchlachterSchmidt <teenageenemanursesinbondage@gmail.com>
2019-01-26 16:51:38 +0000

    Extension Functions with nullable receiver

--- src/main/kotlin/Chapter6/6-12-ExtensionFunctionWithNullableReceiver.kt
+++ src/main/kotlin/Chapter6/6-12-ExtensionFunctionWithNullableReceiver.kt
@@ -1,2 +1,14 @@
 package Chapter6
 
+fun main() {
+
+    fun String?.isNullOrBlank(): Boolean = this == null || this.isBlank()
+
+    var sth: String? = null
+
+    // even if sth is null, the safe call operator is not needed in this case
+    println(sth.isNullOrBlank())
+
+    sth = ""
+    println(sth.isNullOrBlank())
+}
